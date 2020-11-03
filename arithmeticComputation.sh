76227e30215b429cbc004f18ce5d621c828453fa
AmitM0287 <amitmanna0287@gmail.com>
2020-11-04 00:07:39 +0530

    compute op1 a+b*c

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,3 +5,8 @@ echo
 read -p "Enter 1st integer : " a
 read -p "Enter 2nd integer : " b
 read -p "Enter 3rd integer : " c
+
+echo
+#op1 : Compute a+b*c
+op1=`expr $a + $b \* $c`
+echo "op1 : a+b*c = " $op1
